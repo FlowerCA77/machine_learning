eea1584fec59ec2a7c4e9ef611bf284f440c07d9
XhernoPhys CA77 <ca538468142@outlook.com>
2024-09-26 17:03:01 +0800

    handwriting

--- ml_in_action/classification/kNN/simple_example.py
+++ ml_in_action/classification/kNN/simple_example.py
@@ -6,8 +6,8 @@ import classify as kNN_classify
 ds = kNN_gd.create_dataSet()
 group, features = ds[0], ds[1]
 
-X=np.array([0.7, 0.6])
-F=kNN_classify.classify(X, group, features, 5)
+X = np.array([0.7, 0.6])
+F = kNN_classify.classify(X, group, features, 5)
 
 group = np.append(group, np.array([X]), axis=0)
 features = np.append(features, np.array([F]), axis=0)
